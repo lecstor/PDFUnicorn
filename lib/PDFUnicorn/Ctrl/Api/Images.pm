fcfc2294ed557d7d16f0050ff23170e4e942ab28
Jason Galea <jason@lecstor.com>
2013-12-15 01:31:00 +1000

    start on accepting uploaded images

--- lib/PDFUnicorn/Ctrl/Api/Images.pm
+++ lib/PDFUnicorn/Ctrl/Api/Images.pm
@@ -9,4 +9,16 @@ sub uri{ 'images' }
 sub item_schema{ 'Image' }
 sub query_schema{ 'ImageQuery' }
 
+
+sub create {
+	my $self = shift;
+    my $upload = $self->req->upload;
+    my $name = $self->req->params->{'name'};
+    my $base = $self->app->media_directory.'/'.$self->api_key_owner;
+    my $file = $base . '/$name';
+    $upload->move_to($file);
+
+}
+
+
 1;
