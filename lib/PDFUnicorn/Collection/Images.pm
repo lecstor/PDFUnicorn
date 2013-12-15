e4a3f633504b7101a1ca44c52affe85a917c6666
Jason Galea <jason@lecstor.com>
2013-12-15 13:17:52 +1000

    fixed Image schema

--- lib/PDFUnicorn/Collection/Images.pm
+++ lib/PDFUnicorn/Collection/Images.pm
@@ -9,41 +9,22 @@ sub schemas{
         'Image', {
             id => { type => 'string' },
             name => { type => 'string', required => 1 },
-            type => { type => 'string' },
-            data => { type => 'object' },
-            source => { type => 'string' },
+            uri => { type => 'string', required => 1 },
             created => { type => 'datetime', bson => 'time' },
             modified => { type => 'datetime', bson => 'time' },
             public => { type => 'boolean', bson => 'bool' },
             owner => { type => 'string', bson => 'oid' },
-            images => { type => 'object' },
         },
         'ImageQuery', {
             id => { type => 'string' },
             name => { type => 'string' },
-            type => { type => 'string' },
             created => { type => 'datetime', bson => 'time' },
             modified => { type => 'datetime', bson => 'time' },
             public => { type => 'boolean', bson => 'bool' },
-            archived => { type => 'boolean', bson => 'bool' },
             owner => { type => 'string', bson => 'oid' },
         }
     }
 }
-
-#sub create{
-#    my ($self, $data) = @_;
-#    
-#    # validate data here
-#    die 'missing_id' unless $data->{id};
-#    die 'missing_name' unless $data->{name};
-#    die 'missing_data' unless $data->{data};
-#    $data->{type} ||= 'source';
-#            
-#    my $oid = $self->collection->insert($data);
-#    
-#    return $self->find_one({ _id => $oid });
-#}
 
 
 
