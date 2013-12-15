bad484a599bdfd1d5a40cdaa59d5a17bb429a456
Jason Galea <jason@lecstor.com>
2013-12-15 13:18:24 +1000

    enabled image creation via multipart upload.

--- lib/PDFUnicorn/Ctrl/Api/Images.pm
+++ lib/PDFUnicorn/Ctrl/Api/Images.pm
@@ -2,6 +2,9 @@ package PDFUnicorn::Ctrl::Api::Images;
 use Mojo::Base 'PDFUnicorn::Ctrl::Api';
 
 use Mojo::JSON;
+use Mango::BSON ':bson';
+use File::Path 'make_path';
+use URI::Escape;
 
 sub collection{ shift->db_images }
 
@@ -12,12 +15,43 @@ sub query_schema{ 'ImageQuery' }
 
 sub create {
 	my $self = shift;
-    my $upload = $self->req->upload;
-    my $name = $self->req->params->{'name'};
-    my $base = $self->app->media_directory.'/'.$self->api_key_owner;
-    my $file = $base . '/$name';
-    $upload->move_to($file);
+    my $upload = $self->req->upload('image');
+    my $id = $self->req->param('id');
+    my $name = $self->req->param('name');
+    my $filename = uri_escape($name);
 
+    my $base = $self->app->media_directory.$self->api_key_owner;
+    my $file = $base . "/$filename";
+    
+    if ($upload){
+        make_path($base);
+        $upload->move_to($file);
+    } else {
+        die Data::Dumper->Dumper($self->req);
+    }
+    
+    
+    $id ||= bson_oid;
+    
+    my $image_data = {
+        name => $name,
+        uri => "/api/v1/".$self->uri."/$id",
+        id => $id,
+        owner => $self->api_key_owner,
+    };
+    
+    $self->render_later;
+    
+    $self->collection->create($image_data, sub{
+        my ($err, $doc) = @_;
+        #warn 'images api colletion create '.Data::Dumper->Dumper($doc);
+        $self->render( 
+            json => {
+                status => 'ok',
+                data => $doc
+            }
+        );
+    });  
 }
 
 
