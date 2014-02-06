65b5fc822ed832e9e086dd1ee889afb3603d4bcf
Jason Galea <jason@lecstor.com>
2014-02-06 23:47:45 +1000

    format epoch timestamps

--- lib/PDFUnicorn/Template/Alloy.pm
+++ lib/PDFUnicorn/Template/Alloy.pm
@@ -1,24 +1,16 @@
 package PDFUnicorn::Template::Alloy;
 # ABSTRACT: create document from template and data
 use Template::Alloy;
-
+use DateTime;
+use Data::Dumper;
 
 sub new{
     my ($class) = @_;
     
     my $alloy = Template::Alloy->new();
-
-    $alloy->define_vmethod(
-        'text',
-        epoch => sub{
-            my ($value, $format, $offset) = @_;
-            $value += $offset*60;
-            
-            return 'formatted epoch';
-        }
-    );
-
-    return bless { alloy => $alloy }, $class;   
+    my $self = bless { alloy => $alloy }, $class;
+    $self->add_vmethods();
+    return $self;   
 }
 
 sub render{
@@ -27,5 +19,40 @@ sub render{
     $self->{alloy}->process(\$template, $data, \$out);
     return $out;
 }
+
+sub add_vmethods{
+    my ($self) = @_;
+
+    $self->{alloy}->define_vmethod(
+        'text',
+        epoch => sub{
+            my ($epoch, $options) = @_;
+            $options ||= {};
+
+            my $dt = DateTime->from_epoch(
+                epoch => $epoch,
+                time_zone => $options->{tz} || 'UTC',
+                locale => $options->{locale} || 'en_US',
+            );
+            
+            return $self->format_datetime($dt, $options);
+        }
+    );
+
+}
+
+sub format_datetime{
+    my ($self, $dt, $options) = @_;
+    my $date;
+    if ($options->{cldr}){
+        $date = $dt->cldr($options->{cldr});
+    } else {
+        my $format = $options->{strf} || '%x';
+        $date = $dt->strftime($format);
+    }
+    $date =~ s/\s{2,}/ /g;
+    return $date;
+}
+    
 
 1;
