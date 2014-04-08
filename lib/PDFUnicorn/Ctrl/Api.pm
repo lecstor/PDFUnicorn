1de596a467413e7be511880fced123ed59e49e12
Jason Galea <jason@lecstor.com>
2014-04-08 14:19:07 +1000

    enable requests for deleted documents

--- lib/PDFUnicorn/Ctrl/Api.pm
+++ lib/PDFUnicorn/Ctrl/Api.pm
@@ -67,6 +67,10 @@ sub find {
         );
     }
     $query->{owner} = $self->stash->{account_id};
+    
+    if (exists $query->{deleted}){
+        $query->{deleted} = $query->{deleted} ? bson_true : bson_false;
+    }
     delete $query->{_id};
     delete $query->{id};
     
