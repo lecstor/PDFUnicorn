39f674b377cdbedd30e2fb50045c7d10d10301f8
Jason Galea <jason@lecstor.com>
2014-05-04 22:57:54 +1000

    don't load stripe client into template from server side

--- lib/PDFUnicorn/Ctrl/Admin/Stripe/Invoices.pm
+++ lib/PDFUnicorn/Ctrl/Admin/Stripe/Invoices.pm
@@ -13,19 +13,21 @@ use Data::Dumper ('Dumper');
 sub index{
     my $self = shift;
     $self->stash->{error} = undef;
+
+    $self->stash->{stripe_client} = undef;
     
-    $self->render_later;
-        
-    $self->db_stripe_clients->find_one({ owner => $self->stash->{app_user}{_id} }, sub{
-        my ($err, $doc) = @_;
-        
-        $self->app->log->debug($doc || 'no doc');
-        
-        # stash the user's client doc if they are a stripe connect client
-        $self->stash->{stripe_client} = $doc;
-            
-        $self->render();
-    });
+#    $self->render_later;
+#        
+#    $self->db_stripe_clients->find_one({ owner => $self->stash->{app_user}{_id} }, sub{
+#        my ($err, $doc) = @_;
+#        
+#        $self->app->log->debug($doc || 'no doc');
+#        
+#        # stash the user's client doc if they are a stripe connect client
+#        $self->stash->{stripe_client} = $doc;
+#            
+#        $self->render();
+#    });
     
 }
 
